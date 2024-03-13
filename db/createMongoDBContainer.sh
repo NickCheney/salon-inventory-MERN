8b2c282d21d3344c051c61099f460a9f57855292
Nick Cheney <nicheney.9@gmail.com>
2024-03-13 14:35:28 -0700

    Consolidated env vars

--- db/createMongoDBContainer.sh
+++ db/createMongoDBContainer.sh
@@ -1,6 +1,7 @@
-MONGODB_VERSION=7.0-ubi8
+source ../.env
+
 echo Starting MongoDB Container with version $MONGODB_VERSION...
-docker run --name mongodb -d -p 27017:27017 mongodb/mongodb-community-server:$MONGODB_VERSION
+docker run --name mongodb -d -p $DB_PORT:$DB_PORT mongodb/mongodb-community-server:$MONGODB_VERSION
 echo -e "Done.\n"
 
 echo Running db setup...
